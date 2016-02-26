668b0b26f6da443f8da3590384264bdb75e11e25
cahall <123chall@gmail.com>
2016-02-26 17:42:25 -0600

    Getting client on branch

--- Client/src/com/git/cs309/mmoclient/connection/Connection.java
+++ Client/src/com/git/cs309/mmoclient/connection/Connection.java
@@ -7,7 +7,6 @@ import com.git.cs309.mmoclient.packets.PacketHandler;
 import com.git.cs309.mmoserver.connection.AbstractConnection;
 import com.git.cs309.mmoserver.packets.Packet;
 import com.git.cs309.mmoserver.packets.PacketFactory;
-import com.git.cs309.mmoserver.util.EndOfStreamReachedException;
 import com.git.cs309.mmoserver.util.StreamUtils;
 
 public class Connection extends AbstractConnection {
@@ -15,15 +14,13 @@ public class Connection extends AbstractConnection {
 	public Connection(Socket socket) throws IOException {
 		super(socket);
 	}
-
+	
 	@Override
 	public void run() {
 		while (!this.disconnected) {
 			try {
 				Packet packet = PacketFactory.buildPacket(StreamUtils.readBlockFromStream(input), this);
 				PacketHandler.handlePacket(packet);
-			} catch (EndOfStreamReachedException e) {
-				System.exit(0);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
