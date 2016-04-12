be08ccf0f4aafd408b93011dca888310b0de6225
Xirui Wei <weixirui@iastate.edu>
2016-04-11 21:43:59 -0500

    update skills

--- Client/src/com/git/cs309/mmoclient/gui/interfaces/Skills.java
+++ Client/src/com/git/cs309/mmoclient/gui/interfaces/Skills.java
@@ -1,32 +1,36 @@
-package com.git.cs309.mmoclient.gui.characterselection;
+package com.git.cs309.mmoclient.gui.interfaces;
 
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.JFrame;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import com.git.cs309.mmoclient.Client;
-import com.git.cs309.mmoclient.graphics.SpriteDatabase;
+import com.git.cs309.mmoclient.gui.characterselection.SelectionCharacter;
+import com.git.cs309.mmoclient.gui.game.ViewPanel;
 import com.git.cs309.mmoserver.packets.CharacterSelectionDataPacket;
 import com.git.cs309.mmoserver.packets.InterfaceClickPacket;
 
-public class SelectionCharacterComponent extends Component {
-	/**
-	 * 
-	 */
+public final class Skills extends Component {
 	private static final long serialVersionUID = 3409845224182388202L;
-	private final SelectionCharacter character;
+	private final SkillBox skill;
 	private final int index;
 	
-	public SelectionCharacterComponent(final int index) {
+	public Skills(final int index){
 		this.index = index;
-		character = new SelectionCharacter(index);
-		JButton loginButton = new JButton("Log in");
+		skill = new SkillBox(index);
 		
 		this.addMouseListener(new MouseAdapter() {
 			@Override
@@ -52,22 +56,26 @@ public class SelectionCharacterComponent extends Component {
 		});
 	}
 	
-	public void updateSelectionCharacter(CharacterSelectionDataPacket packet) {
-		character.setCharacter(packet);
-	}
-	
-	@Override
 	public void paint(Graphics g) {
-		ImageIcon icon0 = new ImageIcon("data/sprites/gui/f.pvr.ccz.png"); 
-		//ImageIcon button = new ImageIcon("data/sprites/gui/f.pvr.ccz.png");
-		//JButton rectangle=new JButton(button);
-		g.drawImage(icon0.getImage(), 0, 0, getWidth(), getHeight(), 2, 10, 570, 590, null);
-		if (character.hasCharacter()) {
-			//SpriteDatabase.getInstance().getSprite("playerStop");
-			ImageIcon icon1 = new ImageIcon("data/sprites/playerStop.png");
-			g.drawImage(icon1.getImage(), getWidth()/2-50, getHeight()/2-50, getWidth()/2+50, getHeight()/2+50, 0, 0, 96, 96, null);
-			g.drawString(character.getName(), getWidth() / 4, getHeight() / 4 );
+		ImageIcon skill = null;
+		switch (index) {
+		case 0:
+			skill = new ImageIcon("data/sprites/skills/1 (1).png");
+			break;
+		case 1:
+			skill = new ImageIcon("data/sprites/skills/1 (10).png");
+			break;
+		case 2:
+			skill = new ImageIcon("data/sprites/skills/1 (2).png");
+			break;
+		case 3:
+			skill = new ImageIcon("data/sprites/skills/1 (3).png");
+			break;
+		case 4:
+			skill = new ImageIcon("data/sprites/skills/1 (4).png");
+			break;
 		}
+			g.drawImage(skill.getImage(),0,0,getWidth(), getHeight(), null);
 	}
 
 	/**
@@ -76,4 +84,9 @@ public class SelectionCharacterComponent extends Component {
 	public int getIndex() {
 		return index;
 	}
+
+	/*public void updateSkills(SkillsDataPacket packet) {
+		skill.setCharacter(packet);
+	}*/
+	
 }
